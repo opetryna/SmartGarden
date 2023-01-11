fa9a35753df33df31ca508c04d94e16c2565862f
Oleh Petryna <oleh.petryna@outlook.com>
2023-01-11 00:33:18 +0000

    Arduino ADC code added to the repo.

---
+++ adc.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 6; i++) {
+    int val = analogRead(i);
+    Serial.print(val);
+    Serial.print("\t");
+  }
+  Serial.println();
+  delay(1000);
+}
